cdbc42db2d434ab981904694fd58ce8c1717e054
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-25 17:54:30 -0700

    add link to admin/products/ and link to new admin/categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["ADMIN_USER_NAME"], password: ENV["ADMIN_PASSWORD"]
   def show
     @category = Category.all.order(created_at: :desc)
     @products = Product.all.order(created_at: :desc)
