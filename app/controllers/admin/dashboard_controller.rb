4d105bb3dcbd40ce59cb1039b09927f03bb50582
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-25 17:16:54 -0700

    add @category and @products to be passed to show in dashboard_controller and display on admin/dashboard/show

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @category = Category.all.order(created_at: :desc)
+    @products = Product.all.order(created_at: :desc)
   end
 end
