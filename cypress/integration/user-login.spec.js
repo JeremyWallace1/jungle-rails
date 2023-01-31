06148d66766235c773b1ef8e444e713d53250696
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-31 15:12:36 -0700

    add testing for user-login confirm opens signup form, fills out signup form with randomly-generated email and able to login with those credentials also

--- cypress/integration/user-login.spec.js
+++ cypress/integration/user-login.spec.js
@@ -1,4 +1,24 @@
 describe('jungle-rails user_login', () => {
+  function makeEmail() {
+    const strValues = "abcdefg12345";
+    let strEmail = "";
+    let strTmp;
+    for (let i = 0; i < 10; i++) {
+        strTmp = strValues.charAt(Math.round(strValues.length * Math.random()));
+        strEmail = strEmail + strTmp;
+    }
+    strTmp = "";
+    strEmail = strEmail + "@";
+    for (let j = 0; j < 8; j++) {
+        strTmp = strValues.charAt(Math.round(strValues.length * Math.random()));
+        strEmail = strEmail + strTmp;
+    }
+    strEmail = strEmail + ".com"
+    return strEmail;
+  }
+
+  let randomEmail = makeEmail();
+
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
@@ -6,8 +26,28 @@ describe('jungle-rails user_login', () => {
     // we include it in our beforeEach function so that it runs before each test
     cy.visit('/')
   });
-  it('should open the product page when product is clicked on from home page', () => {
-    cy.get(".products article").first().click();
-    cy.get(".product-detail").should("be.visible");
+  it('should open signup form when Signup button clicked', () => {
+    cy.get('a[href="/signup"]').click();
+    cy.get('#user_first_name').should("be.visible");
   });
+  it('should create a new user when Signup form is filled out', () => {
+    cy.get('a[href="/signup"]').click();
+    cy.get('#user_first_name').type('Jeremy');
+    cy.get('#user_last_name').type('Wallace');
+    cy.get('#user_email').type(randomEmail);
+    cy.get('#user_password').type('testing123!');
+    cy.get('#user_password_confirmation').type('testing123!');
+    cy.get('form').submit();
+    cy.get('.nav-link').should('contain.text', 'Signed in as');
+    // cy.get('a[href="/logout"]').click();
+    // cy.get('a[href="/login"]').should("be.visible");
+  });
+  it('should be able to login with email and password credentials of existing user', () => {
+    cy.get('a[href="/login"]').click();
+    cy.get('#email').type(randomEmail);
+    cy.get('#password').type('testing123!');
+    cy.get('form').submit();
+    cy.get('.nav-link').should('contain.text', 'Signed in as');
+  });
+
 });
