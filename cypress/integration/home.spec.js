b72930a17b3fe99b7e171820e2233fbfb7b7f73f
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-31 13:36:59 -0700

    initial commit

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,7 @@
 // <reference types="cypress" />
 
 
-describe('example to-do app', () => {
+describe('jungle-rails home', () => {
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
--- cypress/integration/user-login.spec.js
+++ cypress/integration/user-login.spec.js
@@ -1,4 +1,4 @@
-describe('jungle-rails add_to_cart', () => {
+describe('jungle-rails user_login', () => {
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
@@ -6,8 +6,8 @@ describe('jungle-rails add_to_cart', () => {
     // we include it in our beforeEach function so that it runs before each test
     cy.visit('/')
   });
-  it('should display 1 in the cart when "add" button clicked for a product', () => {
-    cy.get(".btn").first().click({force: true});
-    cy.get(".nav-link").should('contain.text', "My Cart (1)");
+  it('should open the product page when product is clicked on from home page', () => {
+    cy.get(".products article").first().click();
+    cy.get(".product-detail").should("be.visible");
   });
 });
