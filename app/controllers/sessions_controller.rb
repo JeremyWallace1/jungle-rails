90a9536b9d93e6f212ca2667f988fa0fd319dc35
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-30 19:55:27 -0700

    add lower to sessioncontroller create method for email to check user exists

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    # pp user.inspect
+    user = User.where('lower(email) = ?', params[:email].downcase).first
     # If the user exists AND the password entered is correct.
     if user && user.authenticate_with_credentials(params[:email], params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, :uniqueness => { :case_sensitive => false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, presence: true, length: { minimum: 8 }
 
   def authenticate_with_credentials(email, password)
-    # puts "Authenticate_with_credentials method"
-    # pp email
-    # pp password
     if self.email.downcase == email.downcase.strip && self.authenticate(password)
       self
     else
